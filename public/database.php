d1b76bb42afc66f61f156ba8fc2c3f6e33c38235
subl-1me <julio.ugalde404@gmail.com>
2024-12-04 18:52:08 -0700

    Basic params validation for Login requests

--- public/database.php
+++ public/database.php
@@ -1,15 +1,31 @@
 <?php
-
-
-
+define("MYSQL_DB_NAME", "test_practice_reservations");
+define("MYSQL_PASSWORD", "");
+define("MYSQL_USERNAME", "root");
+define("MYSQL_HOST", "localhost");
 function userExists($email){
 
 }
 
 function dbConnect(){
-    $db_name = gotenv("mysql_db_name");
-    echo $db_name
-    return mysqli.connect("")
+    return new mysqli(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DB_NAME);
+}
+
+function findUserByEmail($email){
+    $conn = dbConnect();
+    $sql = "SELECT * FROM admins WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $email);
+    if($stmt->execute()){
+        $result = $stmt->get_result();
+        if($result->num_rows === 0){
+            return null;
+        }
+
+        $userMatch = $result->fetch_assoc();
+        return $userMatch;
+    }
+    return null;
 }
 
 ?>
--- public/valid.php
+++ public/valid.php
@@ -1,9 +1,19 @@
 <?php
-include 'database.php'
+include 'database.php';
 if($_SERVER["REQUEST_METHOD"] === "POST"){
     // get form data
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $userMatch = findUserByEmail($email);
+    if(!$userMatch){
+        echo "User doesn't exist.";
+        return;
+    }
+
+    echo "User found:";
+    print_r($userMatch);
+    
+    // print_r($result);
 }else{
     echo "Invalid recieved data.";
 }
